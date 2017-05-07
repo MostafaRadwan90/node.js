05cc7ab11269dab74145f805c4516e14682b7505
Mostafa Radwan <eng.mosradwan@gmail.com>
2017-05-07 15:39:17 +0200

    new file:   demo-tool.js 	new file:   radwan.js 	new file:   radwan1.js

---
+++ radwan.js
@@ -0,0 +1,13 @@
+var mqtt = require('/usr/local/bin/mqtt');
+var client  = mqtt.connect('mqtt://iot.eclipse.org')
+
+client.on('connect', function () {
+  client.subscribe('radwan')
+  client.publish('radwan', 'Hello mqtt')
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
